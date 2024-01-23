68fd9741d671f856432c6383854da2e8cb77dce2
tj ak <xiomeemi4i@gmail.com>
2024-01-23 17:41:51 +0530

    to check component updation

--- src/components/SecondComponent.js
+++ src/components/SecondComponent.js
@@ -4,7 +4,11 @@ import React, { Component } from "react";
 class SecondComponent extends Component {
   render() {
     return (
+        <div>
         <h1>{this.props.propsSendingsndpge}</h1>
+        <h1>hello</h1>
+        <h1>akshay</h1>
+        </div>
     )
 
   }
